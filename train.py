c1cc83134cd1b9f40ef42bda956119ff737a4099
honeycomb <tarakosuki98525mk2@gmail.com>
2021-06-13 02:47:59 +0900

    add training program

--- train.py
+++ train.py
@@ -1,3 +1,67 @@
-import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D,Input
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.callbacks import CSVLogger
 
-model = tf.keras.applications.vgg16.VGG16(weights='imagenet')
+N_CATEGORIES  = 6
+IMAGE_SIZE = 224
+BATCH_SIZE = 16
+
+train_dir='auged_data/train'
+validation_dir='auged_data/validation'
+file_name='vgg16_coin_fine'
+
+base_model=VGG16(weights='imagenet', include_top=False, 
+                 input_tensor=Input(shape=(224,224,3)))
+
+#add new layers instead of FC networks
+x=base_model.output
+x=GlobalAveragePooling2D()(x)
+x=Dense(1024,activation='relu')(x)
+prediction=Dense(N_CATEGORIES, activation='softmax')(x)
+model=Model(inputs=base_model.input, outputs=prediction)
+
+#fix weights before VGG16 14layers
+for layer in base_model.layers[:15]:
+    layer.trainable=False
+
+model.compile(optimizer=SGD(lr=0.0001,momentum=0.9),
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.summary()
+
+train_datagen = ImageDataGenerator(
+    rescale = 1.0/255,
+    shear_range = 0.2,
+    zoom_range = 0.2,
+    horizontal_flip = True)
+
+validation_datagen = ImageDataGenerator(rescale = 1.0/255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size = (IMAGE_SIZE, IMAGE_SIZE),
+    batch_size = BATCH_SIZE,
+    class_mode = 'categorical',
+    shuffle = True
+)
+
+validation_generator = validation_datagen.flow_from_directory(
+    validation_dir,
+    target_size = (IMAGE_SIZE, IMAGE_SIZE),
+    batch_size = BATCH_SIZE,
+    class_mode = 'categorical',
+    shuffle = True
+)
+
+hist=model.fit_generator(train_generator,
+                         epochs=200,
+                         verbose=1,
+                         validation_data=validation_generator,
+                         callbacks=[CSVLogger(file_name+'.csv')])
+
+#save weights
+model.save(file_name+'.h5')
