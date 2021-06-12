a5d9be6000fb6f7ccb9f524e7ee4d74cf251d84c
honeycomb <tarakosuki98525mk2@gmail.com>
2021-06-13 02:53:12 +0900

    hoge

--- augmentation.py
+++ augmentation.py
@@ -13,14 +13,20 @@ def gen_aug_img(img, method):
         pass
     return gen_images
 
-dir_name = "./images"
-new_dir_name = "./auged_data"
-files = os.listdir(dir_name)
-for file in files:
-    photo = Image.open(os.path.join(dir_name, file))
-    photo_resize = photo.resize((224,224))
-    rotate_images = gen_aug_img(photo_resize, "rotate")
-    for i, image in enumerate(rotate_images):
-        fn = file.split('.')[0] + "_" + str(i+1).zfill(3) + '.jpg'
-        image.save(os.path.join(new_dir_name, fn))
-    photo_resize.save(os.path.join(new_dir_name, file))
+coins = ["1", "5", "10", "50", "100", "500"]
+for coin in coins:
+    dir_name = "./images" + os.sep + coin
+    new_dir_name = "./auged_data"
+    files = os.listdir(dir_name)
+    for file in files:
+        photo = Image.open(os.path.join(dir_name, file))
+        photo_resize = photo.resize((224,224))
+        rotate_images = gen_aug_img(photo_resize, "rotate")
+        for i, image in enumerate(rotate_images):
+            fn = file.split('.')[0] + "_" + str(i+1).zfill(3) + '.jpg'
+            if i%9 == 0:
+                new_dir_name_ = new_dir_name + os.sep + 'validation' + os.sep + coin
+                image.save(os.path.join(new_dir_name_, fn))
+            else:
+                new_dir_name_ = new_dir_name + os.sep + 'train' + os.sep + coin
+                image.save(os.path.join(new_dir_name_, fn))
--- readme.md
+++ readme.md
@@ -2,8 +2,12 @@
 
 ## requirements 
 - Python 3.9.5
-- tensorflow 2.4.1
+- tensorflow-2.4.1
+- tensorflow-gpu-2.4.1
+- cudatoolkit-10.1.243
+- cudnn-7.6.5
 - opencv 4.4.0
+- pillow-8.2.0
 
 ## ハフ変換による円検出
 ハフ変換を用いて円(硬貨)を検出している．
@@ -12,3 +16,6 @@ coin_detection.pyから実行可能
 ## VGG16を用いた硬貨の分類
 VGG16と呼ばれるニューラルネットワークを用いる事で検出した硬貨の種類を分類している．
 ImageNetを学習済み．
+
+## 参考にしたサイト
+- http://labs.eecs.tottori-u.ac.jp/sd/Member/oyamada/OpenCV/html/py_tutorials/py_imgproc/py_houghcircles/py_houghcircles.html
